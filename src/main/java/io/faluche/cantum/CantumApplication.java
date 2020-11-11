1108363f930d88fe63a541872cf74d26e3e67b5c
alexandremoreau <contact@alexandre-moreauL.fr>
2020-11-11 19:43:07 +0100

    initial commit

---
+++ src/main/java/io/faluche/cantum/CantumApplication.java
@@ -0,0 +1,13 @@
+package io.faluche.cantum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CantumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/faluche/cantum/CantumApplicationTests.java
@@ -0,0 +1,13 @@
+package io.faluche.cantum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
